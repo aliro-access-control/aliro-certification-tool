55370f1dede3b80090d731759fb26e6a44e5ca24
Mikael H. Moeller <mikaelhm@apple.com>
2024-01-05 22:38:54 +0000

    Support hex keys as test_parameters

--- test_collections/aliro/support/aliro_test_case.py
+++ test_collections/aliro/support/aliro_test_case.py
@@ -17,7 +17,7 @@
 from typing import Any
 
 from aliro_actuator.trust_framework.endpoint import Endpoint
-from aliro_actuator.trust_framework.key import KeyPair, PublicKey
+from aliro_actuator.trust_framework.key import KeyPair, PrivateKey, PublicKey
 
 from app.test_engine.logger import test_engine_logger as logger
 from app.test_engine.models import TestCase
@@ -85,6 +85,101 @@ class AliroTestCase(TestCase):
 
         return value
 
+    def public_key_from_config(self, parameter_name: str) -> PublicKey:
+        """Get a test parameter as a PublicKey. Must be an Elliptic Curve Public Key.
+        Parameter value must be an encoding in either
+         - hex string (DER format) or
+         - pem string.
+
+        Args:
+            parameter_name (str): key for value in test_parameters
+
+        Raises:
+            AliroTestParameterError: Raised if value is not a string.
+            ValueError:
+                - If the PEM data's structure could not be decoded successfully.
+                - If the hex DER data has invalid length.
+            cryptography.exceptions.UnsupportedAlgorithm: If the serialized key type
+                is not supported by the OpenSSL version cryptography is using.
+            aliro_actuator.trust_framework.errors.InvalidKeyFormatError: If key is not
+            an Elliptic Curve Public Key.
+
+        Returns:
+            PublicKey: Parsed and decoded key.
+        """
+        try:
+            config_bytes = self.bytes_from_config(parameter_name)
+            return PublicKey(config_bytes)
+        except AliroTestParameterError:
+            # This can happen due to test parameter
+            # - not being a string
+            # - not being a valid hex string
+            #
+            # We fall back to parse this as a PEM string.
+            pass
+
+        config_str = self.string_from_config(parameter_name)
+        return PublicKey(config_str)
+
+    def private_key_from_config(
+        self, parameter_name: str, public_key: PublicKey | None = None
+    ) -> PrivateKey:
+        """Get a test parameter as a PrivateKey. Must be an Elliptic Curve Private Key,
+        without passcode.
+
+        Parameter value must be an encoding in either
+         - hex string (DER format, PKCS8 encoding, 138bytes or 32 bytes)
+         - pem string.
+
+        When key is encoded as a 32 byte hex string, the Public key is required to
+        derive the full private key.
+
+        Args:
+            parameter_name (str): key for value in test_parameters
+            public_key (PublicKey, optional): Required when private_key is encoded as
+            32 byte hex string.
+
+        Raises:
+            AliroTestParameterError:
+            - Raised if value is not a string.
+            - If the hex string is 32 bytes and no public_key is provided
+            ValueError: If the PEM data could not be decrypted or if its structure could
+                not be decoded successfully.
+            TypeError: If the key was encrypted with a password.
+            cryptography.exceptions.UnsupportedAlgorithm: If the serialized key type is
+            not supported by the OpenSSL version cryptography is using.
+            aliro_actuator.trust_framework.errors.InvalidKeyFormatError:
+                - If key is not an Elliptic Curve Private Key.
+                - If the hex string is not 138 or 32 bytes
+
+        Returns:
+            PrivateKey: Parsed and decoded key.
+        """
+        try:
+            private_key_bytes = self.bytes_from_config(parameter_name)
+
+            if len(private_key_bytes) == 32:
+                # 32 byte private key requires public key to build full private key
+                if public_key is None:
+                    raise AliroTestParameterError(
+                        "Public Key must be present when parsing, "
+                        "32byte hex encoded DER private key."
+                    )
+                return PrivateKey(private_key_bytes, public_key.as_bytes())
+
+            else:
+                return PrivateKey(private_key_bytes)
+        except AliroTestParameterError:
+            # This can happen due to test parameter
+            # - not being a string
+            # - not being a valid hex string
+            #
+            # We fall back to parse this as a PEM string.
+            pass
+
+        config_str = self.string_from_config(parameter_name)
+        return PrivateKey(config_str)
+
 
 class AliroReaderTestCase(AliroTestCase):
     """Base test case class for Aliro test cases testing readers.
@@ -100,6 +195,11 @@ class AliroReaderTestCase(AliroTestCase):
     used in the test script.
     """
 
+    # Test Parameter keys
+    READER_PUBLIC_KEY_KEY = "dut_reader_public_key"
+    READER_GROUP_ID_KEY = "dut_reader_group_identifier"
+    READER_SUB_GROUP_ID_KEY = "dut_reader_subgroup_identifier"
+
     @classmethod
     def default_test_parameters(self) -> dict[str, Any]:
         """Default test paramters for reader test cases.
@@ -110,12 +210,11 @@ class AliroReaderTestCase(AliroTestCase):
             dict[str, Any]: default test parameters.
         """
         return {
-            "dut_reader_public_key": "-----BEGIN PUBLIC KEY-----\n"
-            "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEOSjzIgGdR1eJO95qD+XhPj5Te5yg\n"
-            "9UnAvS9A95BgJSoKTykRkhV6lctusgJ1lCjADNg0mYxdDqsZLuiHPF007g==\n"
-            "-----END PUBLIC KEY-----\n",
-            "dut_reader_group_identifier": "00113344667799AA00113344667799AA",
-            "dut_reader_subgroup_identifier": "113344667799AA00113344667799AA00",
+            self.READER_PUBLIC_KEY_KEY: "043928f322019d4757893bde6a0fe5e13e3e537b9ca0"
+            "f549c0bd2f40f79060252a0a4f291192157a95cb6eb202759428c00cd834998c5d0eab19"
+            "2ee8873c5d34ee",
+            self.READER_GROUP_ID_KEY: "00113344667799AA00113344667799AA",
+            self.READER_SUB_GROUP_ID_KEY: "113344667799AA00113344667799AA00",
         }
 
     def reader_endpoint(self) -> Endpoint:
@@ -127,32 +226,64 @@ class AliroReaderTestCase(AliroTestCase):
         """
 
         logger.info("Generating User Device Key Pair")
-        user_device_keypair = KeyPair()
+        user_device_key_pair = KeyPair()
 
         # Reader Device UT
         logger.info("Loading DUT Reader info from test_parameters in project config.")
 
-        reader_public_key_str = self.string_from_config("dut_reader_public_key")
-        logger.info(f"Using Reader Public Key: {reader_public_key_str}")
+        # Public Key
+        logger.info(f"Loading public key from '{self.READER_PUBLIC_KEY_KEY}'")
+        reader_public_key = self.public_key_from_config(self.READER_PUBLIC_KEY_KEY)
+        logger.info(
+            f"Using Reader Public Key(hex): \n{reader_public_key.as_bytes().hex()}"
+        )
+        logger.info(f"Using Reader Public Key(PEM): \n{reader_public_key.as_pem()}")
 
-        reader_group_identifier = self.bytes_from_config("dut_reader_group_identifier")
+        # Group Identifier
+        logger.info(
+            f"Loading Reader group identifier from '{self.READER_GROUP_ID_KEY}'"
+        )
+        reader_group_identifier = self.bytes_from_config(self.READER_GROUP_ID_KEY)
         logger.info(f"Using Reader Group Identifier: {reader_group_identifier.hex()}")
 
+        # Sub-Group Identifier
+        logger.info(
+            f"Loading Reader sub-group identifier from '{self.READER_SUB_GROUP_ID_KEY}'"
+        )
         reader_sub_group_identifier = self.bytes_from_config(
-            "dut_reader_group_identifier"
+            self.READER_SUB_GROUP_ID_KEY
         )
         logger.info(
             f"Using Reader Sub-Group Identifier: {reader_sub_group_identifier.hex()}"
         )
 
         return Endpoint(
-            user_device_keypair,
-            PublicKey(reader_public_key_str),
-            [reader_group_identifier + reader_sub_group_identifier],
+            user_device_key_pair=user_device_key_pair,
+            reader_public_key=reader_public_key,
+            reader_identifier=[reader_group_identifier + reader_sub_group_identifier],
         )
 
 
 class AliroUserDeviceTestCase(AliroTestCase):
+    """Base test case class for Aliro test cases testing User Devices.
+
+    User Device test cases will simulate a reader, using this information:
+    - KeyPair (Private and Public keys)
+    - group identifier
+    - sub group identifier.
+
+    These can be set in the test_paramters as part of the project configuration.
+
+    This base class will handle the loading and parsing of these values, so they can be
+    used in the test script.
+    """
+
+    # Test Parameter keys
+    READER_PRIVATE_KEY_KEY = "th_reader_private_key"
+    READER_PUBLIC_KEY_KEY = "th_reader_public_key"
+    READER_GROUP_ID_KEY = "th_reader_group_identifier"
+    READER_SUB_GROUP_ID_KEY = "th_reader_sub_group_identifier"
+
     @classmethod
     def default_test_parameters(self) -> dict[str, Any]:
         """Default test paramters for user device test cases.
@@ -163,17 +294,13 @@ class AliroUserDeviceTestCase(AliroTestCase):
             dict[str, Any]: default test parameters.
         """
         return {
-            "th_reader_private_key": "-----BEGIN EC PRIVATE KEY-----\n"
-            "MHcCAQEEIIrv3/jVtHqpo+26x6NF7SIhAhUS/VWr3juO4PIIlSaToAoGCCqGSM49\n"
-            "AwEHoUQDQgAEOSjzIgGdR1eJO95qD+XhPj5Te5yg9UnAvS9A95BgJSoKTykRkhV6\n"
-            "lctusgJ1lCjADNg0mYxdDqsZLuiHPF007g==\n"
-            "-----END EC PRIVATE KEY-----",
-            "th_reader_public_key": "-----BEGIN PUBLIC KEY-----\n"
-            "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEOSjzIgGdR1eJO95qD+XhPj5Te5yg\n"
-            "9UnAvS9A95BgJSoKTykRkhV6lctusgJ1lCjADNg0mYxdDqsZLuiHPF007g==\n"
-            "-----END PUBLIC KEY-----\n",
-            "th_reader_group_identifier": "00113344667799AA00113344667799AA",
-            "th_reader_sub_group_identifier": "113344667799AA00113344667799AA00",
+            self.READER_PRIVATE_KEY_KEY: "8aefdff8d5b47aa9a3edbac7a345ed2221021512fd55"
+            "abde3b8ee0f208952693",
+            self.READER_PUBLIC_KEY_KEY: "043928f322019d4757893bde6a0fe5e13e3e537b9ca0f5"
+            "49c0bd2f40f79060252a0a4f291192157a95cb6eb202759428c00cd834998c5d0eab192ee8"
+            "873c5d34ee",
+            self.READER_GROUP_ID_KEY: "00113344667799AA00113344667799AA",
+            self.READER_SUB_GROUP_ID_KEY: "113344667799AA00113344667799AA00",
         }
 
     def th_reader_keypair(self) -> KeyPair:
@@ -184,13 +311,32 @@ class AliroUserDeviceTestCase(AliroTestCase):
         Returns:
             KeyPair: Key pair for TH reader.
         """
-        reader_private_key = self.string_from_config("th_reader_private_key")
-        logger.info(f"TH Using Reader Private Key: \n{reader_private_key}")
+        logger.info("Loading key pair for Test Harness use on simulated Reader.")
 
-        reader_public_key = self.string_from_config("th_reader_public_key")
-        logger.info(f"TH Using Reader Public Key: \n{reader_public_key}")
+        # Public Key
+        logger.info(f"Loading public key from '{self.READER_PUBLIC_KEY_KEY}'")
+        reader_public_key = self.public_key_from_config(self.READER_PUBLIC_KEY_KEY)
+        logger.info(
+            f"TH Using Reader Public Key(hex): \n{reader_public_key.as_bytes().hex()}"
+        )
+        logger.info(f"TH Using Reader Public Key(pem): \n{reader_public_key.as_pem()}")
 
-        return KeyPair(private_key=reader_private_key, public_key=reader_public_key)
+        # Private Key
+        logger.info(f"Loading private key from '{self.READER_PRIVATE_KEY_KEY}'")
+        reader_private_key = self.private_key_from_config(
+            self.READER_PRIVATE_KEY_KEY, public_key=reader_public_key
+        )
+        logger.info(
+            f"TH Using Reader Private Key(hex): \n{reader_private_key.as_bytes().hex()}"
+        )
+        logger.info(
+            f"TH Using Reader Private Key(pem): \n{reader_private_key.as_pem()}"
+        )
+
+        return KeyPair(
+            private_key=reader_private_key,
+            public_key=reader_public_key,
+        )
 
     def th_group_identifier(self) -> bytes:
         """Load TH Reader group identifier from test parameters.
@@ -200,7 +346,12 @@ class AliroUserDeviceTestCase(AliroTestCase):
         Returns:
             bytes: group identifier
         """
-        return self.bytes_from_config("th_reader_group_identifier")
+        logger.info(
+            f"Loading Reader group identifier from '{self.READER_GROUP_ID_KEY}'"
+        )
+        group_id = self.bytes_from_config(self.READER_GROUP_ID_KEY)
+        logger.info(f"Using Reader group identifier(hex): {group_id.hex()}")
+        return group_id
 
     def th_sub_group_identifier(self) -> bytes:
         """Load TH Reader sub-group identifier from test parameters.
@@ -210,4 +361,9 @@ class AliroUserDeviceTestCase(AliroTestCase):
         Returns:
             bytes: sub-group identifier
         """
-        return self.bytes_from_config("th_reader_sub_group_identifier")
+        logger.info(
+            f"Loading Reader sub-group identifier from '{self.READER_SUB_GROUP_ID_KEY}'"
+        )
+        sub_group_id = self.bytes_from_config(self.READER_SUB_GROUP_ID_KEY)
+        logger.info(f"Using Reader sub-group identifier(hex): {sub_group_id.hex()}")
+        return sub_group_id
