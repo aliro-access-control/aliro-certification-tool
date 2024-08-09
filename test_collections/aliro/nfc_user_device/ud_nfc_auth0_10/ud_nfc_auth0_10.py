1c62b980c837033b07859d3ecbb65f282a86bcda
Mike Jongen <mike.jongen@nxp.com>
2024-08-09 16:14:00 +0200

    AUTH0 10 and 11 tests

--- test_collections/aliro/nfc_user_device/ud_nfc_auth0_10/ud_nfc_auth0_10.py
+++ test_collections/aliro/nfc_user_device/ud_nfc_auth0_10/ud_nfc_auth0_10.py
@@ -20,11 +20,11 @@ from app.user_prompt_support import OptionsSelectPromptRequest, UserPromptSuppor
 from ...support.aliro_test_case import AliroUserDeviceTestCase, log_errors
 
 
-class UD_NFC_STDTXN_10(AliroUserDeviceTestCase, UserPromptSupport):
+class UD_NFC_AUTH0_10(AliroUserDeviceTestCase, UserPromptSupport):
     metadata = {
-        "public_id": "UD-NFC-STDTXN-1.0",
+        "public_id": "UD-NFC-AUTH0-1.0",
         "version": "0.0.1",
-        "title": "UD-NFC-STDTXN-1.0",
+        "title": "UD-NFC-AUTH0-1.0",
         "description": """Verify conformance of User Device UT in AUTH0 command.""",
     }
 
@@ -56,7 +56,7 @@ class UD_NFC_STDTXN_10(AliroUserDeviceTestCase, UserPromptSupport):
         ]
 
     async def setup(self) -> None:
-        logger.info("This is a test case setup")
+        logger.info("UD_NFC_AUTH0_10 setup")
         # load parameters from project config
         group_id = self.th_group_identifier()
         sub_group_id = self.th_sub_group_identifier()
@@ -112,5 +112,5 @@ class UD_NFC_STDTXN_10(AliroUserDeviceTestCase, UserPromptSupport):
         self.next_step()
 
     async def cleanup(self) -> None:
-        logger.info("UD_NFC_STDTXN_10 Cleanup")
+        logger.info("UD_NFC_AUTH0_10 Cleanup")
         await self.reader.transaction_termination()
