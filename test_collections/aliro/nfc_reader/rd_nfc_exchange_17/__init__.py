7a966ebb592bafaa17fe0bf6b6f2ced00932cf3a
Mike Jongen <mike.jongen@nxp.com>
2024-09-13 17:29:43 +0200

    remove 17 for now, does not work yet

--- test_collections/aliro/nfc_reader/rd_nfc_exchange_17/__init__.py
+++ test_collections/aliro/nfc_reader/rd_nfc_exchange_17/__init__.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from .rd_nfc_exchange_17 import RD_NFC_EXCHANGE_17
+# from .rd_nfc_exchange_17 import RD_NFC_EXCHANGE_17
