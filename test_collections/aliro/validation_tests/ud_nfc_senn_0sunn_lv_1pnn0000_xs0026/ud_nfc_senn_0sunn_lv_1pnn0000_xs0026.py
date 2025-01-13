c0bef1b171cdc7513463b51df8e4cb578540e49d
Mircea Caprioru <mircea.capriuoru@nxp.com>
2025-01-13 15:31:07 +0200

    add ud validation tests

---
+++ test_collections/aliro/validation_tests/ud_nfc_senn_0sunn_lv_1pnn0000_xs0026/ud_nfc_senn_0sunn_lv_1pnn0000_xs0026.py
@@ -0,0 +1,141 @@
+from binascii import hexlify
+
+from aliro_actuator.access_protocol.apdu import (
+    INS,
+    AuthenticationPolicy,
+)
+from aliro_actuator.access_protocol.defines import (
+    EXPEDITED_PHASE_AID,
+    TransportProtocol,
+    PROTOCOL_VERSION
+)
+from aliro_actuator.access_protocol.user_device import UserDevice, UserSessionState
+from aliro_actuator.transport_protocol.errors import NoDeviceConnectedError
+from aliro_actuator.trust_framework.key import KeyPair
+from app.test_engine.logger import test_engine_logger as logger
+from app.test_engine.models import TestStep
+from app.user_prompt_support import OptionsSelectPromptRequest, UserPromptSupport
+
+from ...support.aliro_test_case import AliroReaderTestCase, log_errors
+
+
+class UD_NFC_SENN_0SUNN_LV_1PNN0000_XS0026(AliroReaderTestCase, UserPromptSupport):
+    metadata = {
+        "public_id": "UD_NFC_SENN_0SUNN_LV_1PNN0000_XS0026",
+        "version": "0.0.1",
+        "title": "UD_NFC_SENN_0SUNN_LV_1PNN0000_XS0026",
+        "description": """Verify conformance of UD NFC flow.""",
+    }
+
+    reader_ePuBK = bytes.fromhex(
+        "049696afe33de58b7d3253d1cba86d14147c16d455e8"
+        "a27373b38d454af21b70e75e13ebc6d55743ba6a6ffc"
+        "4ed37a55515a9346fdae311f60be30421fa6dc61c5"
+    )
+    reader_ePrivK = bytes.fromhex(
+        "3c0f74114cd2a021e8066efbaa31dbb97ef0054272192606fd96633a04f66214"
+    )
+    transaction_identifier = bytes.fromhex("4165A83667AD0AF5AB115247424822E0")
+
+    @classmethod
+    def pics(cls) -> set[str]:
+        return set(
+            [
+                "",  # PICS in preparation
+            ]
+        )
+
+    def create_test_steps(self) -> None:
+        self.test_steps = [
+            TestStep("Step1: Initialization"),
+            TestStep("Step2: Set to polling mode"),
+            TestStep("Step3: Transaction initiation"),
+            TestStep("Step4: Send/Receive AUTH0 command/response"),
+            TestStep("Step5: Send/Receive LOAD_CERT command/response"),
+            TestStep("Step6: Send/Receive AUTH1 command/response"),
+            TestStep("Step7: Send/Receive EXCHANGE command/response"),
+        ]
+
+    async def setup(self) -> None:
+        logger.info("This is a test case setup")
+        # load parameters from project config
+        group_id = self.th_group_identifier()
+        sub_group_id = self.th_sub_group_identifier()
+        key = self.th_reader_keypair()
+
+        # Initialize Aliro NFC Reader
+        self.reader = Reader(
+            transport_protocol=TransportProtocol.NFC,
+            reader_group_identifier=group_id,
+            reader_group_sub_identifier=sub_group_id,
+            reader_key=key,
+            transaction_identifier_list=[self.transaction_identifier],
+            ephemeral_key_list=[KeyPair(self.reader_ePrivK, self.reader_ePuBK)],
+        )
+
+    @log_errors
+    async def execute(self) -> None:
+        # Test step 1
+        # Done in setup
+        self.next_step()
+
+        # Test step 2
+        # Display pop-up to put the User Device UT on the TH
+        await self.send_prompt_request(
+            OptionsSelectPromptRequest(
+                prompt="Tap User Device on the Test Harness NFC", options={"OK": 1}
+            )
+        )
+        self.next_step()
+
+        # Test step 3
+        try:
+            await self.reader.transaction_initiation()  # including select
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+
+        # Test step 4
+        try:
+            await self.reader.handle_auth0(
+                transaction_type=Transaction.STANDARD,
+                authentication_policy=AuthenticationPolicy.USER_DEVICE,
+            )
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+
+        # Test step 5
+        try:
+            await self.reader.handle_load_cert()
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+
+        # Test step 6
+        try:
+            await self.reader.handle_auth1(
+                expected_response=Auth1Response.CREDENTIAL_PUBLIC_KEY
+            )
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+
+        # Test step 7
+        try:
+            await self.reader.handle_exchange(
+                False, reader_status=ReaderStatus.HARDWARE_ISSUE
+            )
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+
+    async def cleanup(self) -> None:
+        logger.info("UD_NFC_SENN_0SUNN_LV_1PNN0000_XS0026 Cleanup")
+        await self.reader.transaction_termination()
+
