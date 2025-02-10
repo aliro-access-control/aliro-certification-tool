8ff5616b20b155b4e8d07fcee39937c3952cf7c7
Mircea Caprioru <mircea.capriuoru@nxp.com>
2025-02-10 19:38:12 +0200

    use dut_reader_group_identifier_issuer for load cert

--- test_collections/aliro/validation_tests/rd_nfc_SENN_0SRRN_LV_1PNN0000_XSR/rd_nfc_SENN_0SRRN_LV_1PNN0000_XSR.py
+++ test_collections/aliro/validation_tests/rd_nfc_SENN_0SRRN_LV_1PNN0000_XSR/rd_nfc_SENN_0SRRN_LV_1PNN0000_XSR.py
@@ -56,10 +56,10 @@ class RD_NFC_SENN_0SRRN_LV_1PNN0000_XSR(AliroReaderTestCase, UserPromptSupport):
 
     async def setup(self) -> None:
         logger.info("RD_NFC_SENN_0SRRN_LV_1PNN0000_XSR setup")
-        access_credential = self.reader_access_credential()
+        self.access_credential = self.reader_access_credential(add_issuer_public_key=True)
         self.userdevice = UserDevice(
             transport_protocol=TransportProtocol.NFC,
-            access_credentials=[access_credential],
+            access_credentials=[self.access_credential],
             mailbox=0x20,
             ephemeral_key_list=[KeyPair(self.endpoint_ePrivK, self.endpoint_ePuBK)],
         )
@@ -68,6 +68,16 @@ class RD_NFC_SENN_0SRRN_LV_1PNN0000_XSR(AliroReaderTestCase, UserPromptSupport):
     async def execute(self) -> None:
         # Test step 1
         # Done in setup
+        issuer_group_id = self.access_credential.reader_id_key_list[1][0]
+        prompt = "Set the reader_group_identifier of the reader device to: {}\n".format(hexlify(issuer_group_id))
+        prompt += "to the Access Credential of the reader device\n"
+
+        await self.send_prompt_request(
+            OptionsSelectPromptRequest(
+                prompt=prompt,
+                options={"OK": 1},
+            )
+        )
         self.next_step()
 
         # Test step 2
