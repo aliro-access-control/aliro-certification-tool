b1208b0dbba22450fd1ab7146b92edb2afb7fb35
Mircea Caprioru <mircea.capriuoru@nxp.com>
2025-03-24 16:33:09 +0200

    add nfc_ud_neg_auth0_wrong_p1p2 test

---
+++ test_collections/aliro/nfc_user_device/nfc_ud_neg_auth0_wrong_p1p2/nfc_ud_neg_auth0_wrong_p1p2.py
@@ -0,0 +1,152 @@
+from aliro_actuator.access_protocol.apdu import (
+    Auth1Response,
+    AuthenticationPolicy,
+    Transaction,
+)
+from aliro_actuator.access_protocol.defines import (
+    EXPEDITED_PHASE_AID,
+    TransportProtocol,
+    PROTOCOL_VERSION,
+)
+from aliro_actuator.access_protocol.errors import (
+    AccessProtocolError,
+    InvalidResponseError,
+    InvalidStatusError,
+)
+from aliro_actuator.access_protocol.reader import Reader
+from aliro_actuator.trust_framework.key import KeyPair
+from app.test_engine.logger import test_engine_logger as logger
+from app.test_engine.models import TestStep
+from app.user_prompt_support import OptionsSelectPromptRequest, UserPromptSupport
+
+from ...support.aliro_test_case import AliroUserDeviceTestCase, log_errors
+
+
+class NEG_AUTH0_WRONG_P1P2(AliroUserDeviceTestCase, UserPromptSupport):
+    metadata = {
+        "public_id": "NEG_AUTH0_WRONG_P1P2",
+        "version": "0.0.1",
+        "title": "NEG_AUTH0_WRONG_P1P2",
+        "description": """Verify conformance of User Device UT in AUTH0 command.""",
+    }
+
+    reader_ePuBK = bytes.fromhex(
+        "049696afe33de58b7d3253d1cba86d14147c16d455e8"
+        "a27373b38d454af21b70e75e13ebc6d55743ba6a6ffc"
+        "4ed37a55515a9346fdae311f60be30421fa6dc61c5"
+    )
+    reader_ePrivK = bytes.fromhex(
+        "3c0f74114cd2a021e8066efbaa31dbb97ef0054272192606fd96633a04f66214"
+    )
+    transaction_identifier = bytes.fromhex("4165A83667AD0AF5AB115247424822E0")
+
+    @classmethod
+    def pics(cls) -> set[str]:
+        return set(
+            [
+                "",  # PICS in preparation
+            ]
+        )
+
+    def create_test_steps(self) -> None:
+        self.test_steps = [
+            TestStep("Step1: Initialization"),
+            TestStep("Step2: Set to polling mode"),
+            TestStep("Step3: Set the User Device UT"),
+            TestStep("Step4: Send/Receive Select command/response"),
+            TestStep("Step5: Send/Receive AUTH0 command/response"),
+            TestStep("Step6: Send/Receive CONTROL FLOW command/response"),
+        ]
+
+    async def setup(self) -> None:
+        logger.info("NEG_AUTH0_WRONG_P1P2 setup")
+        # load parameters from project config
+        group_id = self.th_group_identifier()
+        sub_group_id = self.th_sub_group_identifier()
+        key = self.th_reader_keypair()
+
+        # Initialize Aliro NFC Reader
+        self.reader = Reader(
+            transport_protocol=TransportProtocol.NFC,
+            reader_group_identifier=group_id,
+            reader_group_sub_identifier=sub_group_id,
+            reader_key=key,
+            transaction_identifier_list=[self.transaction_identifier],
+            ephemeral_key_list=[KeyPair(self.reader_ePrivK, self.reader_ePuBK)],
+        )
+
+    @log_errors
+    async def execute(self) -> None:
+        # Test step 1
+        # Done in setup
+        self.next_step()
+
+        # Test step 2
+        # Display pop-up to put the User Device UT on the TH
+        await self.send_prompt_request(
+            OptionsSelectPromptRequest(
+                prompt="Tap User Device on the Test Harness NFC", options={"OK": 1}
+            )
+        )
+        self.next_step()
+
+        # Test step 3
+        await self.reader.setup_connection()  # up to RATS command/ ATS response
+        self.reader.start_new_session()
+        self.next_step()
+
+        # Test step 4
+        try:
+            await self.reader.handle_select(aid=EXPEDITED_PHASE_AID)
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+
+        # Test step 5
+        try:
+            data_tlv: list[tuple[int, bytes | list]] = [
+                (Auth0.COMMAND_TAG, transaction_type.to_bytes(1, "big")),
+                (Auth0.AUTHENTICATION_POLICY_TAG, authentication_policy.to_bytes(1, "big")),
+                (Auth0.ETPV_TAG, protocol_version.to_bytes(2, "big")),
+                (Auth0.READER_EPUBK_TAG, reader_epubk), 
+                (Auth0.TRANSACTION_ID_TAG, transaction_identifier),
+                (Auth0.READER_IDENTIFIER_TAG, reader_identifier),
+            ]
+            data = TLV(data_tlv)
+            command = self.create_command(
+                cla=0x80,
+                ins=INS.AUTH0,
+                p1=0x01,
+                p2=0x02,
+                data=bytes(data.to_bytes()),
+                le=0x00,
+            )
+            response = await self.apdu.handle_chaining_send_command(
+            "AUTH0", command, self.reader.transport_protocol
+            )
+            response = self.apdu.parse_response(response, INS.AUTH0)
+            self.mark_step_failure("Wrong P1/P2 sent, but it was accepted as a valid")
+            return
+        except InvalidStatusError as error:
+            logger.info(
+                "Received error status (as expected), status received: 0x{:04x}".format(
+                    error.status
+                )
+            )
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+        
+        # Test step 6
+        try:
+            await self.reader.handle_control_flow(S2.NONE)
+        except (AccessProtocolError, InvalidResponseError) as error:
+            self.mark_step_failure(str(error))
+            return
+        self.next_step()
+
+    async def cleanup(self) -> None:
+        logger.info("NEG_AUTH0_WRONG_P1P2 Cleanup")
+        await self.reader.transaction_termination()
