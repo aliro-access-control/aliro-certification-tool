fbe38ad51da561382304bf3343729f52ad49a929
Mircea Caprioru <mircea.capriuoru@nxp.com>
2025-03-24 18:45:40 +0200

    add nfc_rdr_standard_cert_in_load_cert_with_chaining test

--- test_collections/aliro/nfc_reader/nfc_rdr_standard_cert_in_load_cert_with_chaining/__init__.py
+++ test_collections/aliro/nfc_reader/nfc_rdr_standard_cert_in_load_cert_with_chaining/__init__.py
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from .rd_nfc_stdtxn_40 import RD_NFC_STDTXN_40
+from .nfc_rdr_standard_cert_in_load_cert_with_chaining import NFC_RDR_STANDARD_CERT_IN_LOAD_CERT_WITH_CHAINING
