f1f46fab75932a296b371f244afd70af35d7d202
Mircea Caprioru <mircea.capriuoru@nxp.com>
2025-04-04 15:39:23 +0300

    add test nfc_ud_neg_standard_cert_in_load_cert_with_chaining_incorrect_format

---
+++ test_collections/aliro/nfc_user_device/nfc_ud_neg_standard_cert_in_load_cert_with_chaining_incorrect_format/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2023 Project Aliro Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from .nfc_ud_neg_standard_cert_in_load_cert_with_chaining_incorrect_format import NFC_UD_NEG_STANDARD_CERT_IN_LOAD_CERT_WITH_CHAINING_INCORRECT_FORMAT
