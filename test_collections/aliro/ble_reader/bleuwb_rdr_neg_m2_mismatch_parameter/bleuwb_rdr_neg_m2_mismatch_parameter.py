c7365501620907d6e822e87266be917b3e8718a6
Mircea Caprioru <mircea.capriuoru@nxp.com>
2025-06-17 17:35:52 +0300

    fix cleanup name

--- test_collections/aliro/ble_reader/bleuwb_rdr_neg_m2_mismatch_parameter/bleuwb_rdr_neg_m2_mismatch_parameter.py
+++ test_collections/aliro/ble_reader/bleuwb_rdr_neg_m2_mismatch_parameter/bleuwb_rdr_neg_m2_mismatch_parameter.py
@@ -272,7 +272,7 @@ class BLEUWB_RDR_NEG_M2_MISMATCH_PARAMETER(AliroReaderTestCase, UserPromptSuppor
 
 
     async def cleanup(self) -> None:
-        logger.info("BLEUWB_RDR_RANGING_SUSPEND Cleanup")
+        logger.info("BLEUWB_RDR_NEG_M2_MISMATCH_PARAMETER Cleanup")
         try:
             await self.userdevice.transaction_termination()
         except NoDeviceConnectedError:
