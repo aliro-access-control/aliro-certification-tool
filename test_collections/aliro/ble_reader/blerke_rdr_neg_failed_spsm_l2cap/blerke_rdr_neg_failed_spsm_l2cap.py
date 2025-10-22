b6857e6e08755fbfef47acbc32aa4a3cca660b5a
Bogdan <bogdan.sandu@nxp.com>
2025-10-22 14:52:39 +0300

    Save the wrong psm value used by the user device in the transport protocol

--- test_collections/aliro/ble_reader/blerke_rdr_neg_failed_spsm_l2cap/blerke_rdr_neg_failed_spsm_l2cap.py
+++ test_collections/aliro/ble_reader/blerke_rdr_neg_failed_spsm_l2cap/blerke_rdr_neg_failed_spsm_l2cap.py
@@ -121,6 +121,7 @@ class BLERKE_RDR_NEG_FAILED_SPSM_L2CAP(AliroReaderTestCase, UserPromptSupport):
             else:
                 wrong_spsm[1] = 0x80
             logger.debug("Setup l2cap connection with wrong SPSM value")
+            self.userdevice.transport_protocol.spsm = wrong_spsm
             await self.userdevice.transport_protocol.driver.register_le_cb_callback()
             await self.userdevice.transport_protocol.driver.register_le_psm(wrong_spsm)
             await self.userdevice.transport_protocol.driver.connect_le_psm(
